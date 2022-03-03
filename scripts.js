6ee98db9d759d0527b937479fb438fa947b5b81d
Taylor Harris <th@timmermanngroup.com>
2022-03-02 23:15:52 -0600

    Create our word array

---
+++ scripts.js
@@ -0,0 +1,16 @@
+var words = [
+    "quizzify",
+    "jukebox",
+    "lightbulb",
+    "radio",
+    "satellite",
+    "meteor",
+    "galaxy",
+    "stars",
+    "penguin",
+    "fox"
+]
+
+var word = words[Math.floor(Math.random() * words.length)]
+
+console.log(word)
