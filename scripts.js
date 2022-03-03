90009b391133374a259e15ca626f66245a7bc761
Taylor Harris <th@timmermanngroup.com>
2022-03-02 23:27:29 -0600

    Create answer array and get remaining letter word length

--- scripts.js
+++ scripts.js
@@ -14,3 +14,11 @@ var words = [
 var word = words[Math.floor(Math.random() * words.length)]
 
 console.log(word)
+
+var answerArray = []
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+}
+
+var remainingLetters = word.length
+
